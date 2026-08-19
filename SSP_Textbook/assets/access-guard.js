d6d0ac42f0cc0ba6ce679e9db285398a23bb94ac
Mochizuki-TUS <mochizuki@rs.tus.ac.jp>
2026-08-20 03:40:37 +0900

    Add files via upload

---
+++ SSP_Textbook/assets/access-guard.js
@@ -0,0 +1,16 @@
+(function () {
+  var ACCESS_KEY = 'sspTextbookAccess';
+  var ACCESS_VALUE = 'verified-2026-08';
+  var allowed = false;
+
+  try {
+    allowed = sessionStorage.getItem(ACCESS_KEY) === ACCESS_VALUE;
+  } catch (e) {
+    allowed = false;
+  }
+
+  if (!allowed) {
+    document.documentElement.style.display = 'none';
+    window.location.replace('../education.html#ssp-textbook');
+  }
+})();
